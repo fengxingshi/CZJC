faa2ba3a635528c91888008e4687b0e075a55eb7
yanshn <yanshn@qq.com>
2015-08-03 16:48:44 +0800

    增加Oracle监测列表页

---
+++ CZJC/Controllers/OracleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CZJC.Controllers
+{
+    public class OracleController : Controller
+    {
+        // GET: Oracle
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
